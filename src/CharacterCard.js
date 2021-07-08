3aa7888217f11906e3528975f83a87f9091e9129
6210110171 <sunawinatpat@hotmail.com>
2021-07-08 11:27:34 +0700

    4. Components & Props

---
+++ src/CharacterCard.js
@@ -0,0 +1,7 @@
+import logo from './logo.svg';
+
+export default function CharacterCard(props) {
+    return (
+        <div className="card">{props.value}</div>
+    )
+}
---
+++ src/WordCard.js
@@ -0,0 +1,11 @@
+import CharacterCard from './CharacterCard';
+import logo from './logo.svg';
+export default function WordCard(props){
+    return (
+        <div>
+            {
+                Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i}/>)
+            }
+        </div>
+    )
+}
