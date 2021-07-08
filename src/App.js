efcc8413615c362ca125ef6d8249805ee96947a1
6210110171 <sunawinatpat@hotmail.com>
2021-07-08 11:38:45 +0700

    3. Hello world

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import CharacterCard from './CharacterCard';
-import WordCard from './WordCard';
 
-const word = "Hello";
 function App() {
   return (
-    <div>
-      <WordCard value={word}/>
-    </div>
+  <div>
+  Hello World
+  </div>
   );
-}
-
-export default App;
+ }
+ export default App;
